35b102102226d8762b6d65d84d69dcbe33cae171
deliailu27 <54033364+deliailu27@users.noreply.github.com>
2022-08-24 14:28:55 +0100

    update

--- src/routers/books.js
+++ src/routers/books.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const { restart } = require("nodemon");
 const router = express.Router();
 const db = require("../../db");
+const { response } = require("../server");
 
 router.get("/", async (req, res) => {
   console.log("type", req.query.type);
@@ -14,6 +16,22 @@ router.get("/", async (req, res) => {
   console.log(sqlQuery);
   const qResult = await db.query(sqlQuery, params);
   res.json({ books: qResult.rows });
+});
+
+router.get("/:id", async (req, res) => {
+  const sqlQuery = `select * from books while id = ${req.params.id}`;
+  console.log("query:", sqlQuery);
+  const result = await db.query(sqlQuery);
+  res.json(result);
+});
+
+router.post("/", async (req, res) => {
+  const newBook = { ...req.body };
+  console.log("newBook:", newBook);
+  let sqlQuery = `insert into books(title, type, author, topic, publicationDate, pages) values ('${newBook.title}','${newBook.type}','${newBook.author}','${newBook.topic}','${newBook.publicationdate}',${newBook.pages})`;
+  console.log("query:", sqlQuery);
+  await db.query(sqlQuery);
+  res.status(201).json(newBook);
 });
 
 module.exports = router;
